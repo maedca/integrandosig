ec448ba09c910f983f736c736972817b1c72b501
Manuel Camacho <maedca@gmail.com>
2019-03-15 13:22:53 -0400

    layout

--- integrando/resources/views/layouts/partials/nav.blade.php
+++ integrando/resources/views/layouts/partials/nav.blade.php
@@ -1,43 +1,47 @@
-<section class="cover text-center">
-    <nav class="navbar navbar-toggleable-sm navbar-trans navbar-inverse">
-        <div class="container">
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler-1" aria-controls="navbarToggler-1" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark my-nav">
+    <div class="d-flex flex-grow-1">
+        <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
+        <a class="navbar-brand d-none d-md-block" href="#">
+            <img src="{{asset('images/logo.svg')}}" alt="" style="width: 100%;transform: translateX(70px) translateY(50px) " class="">
+        </a>
+        <a class="navbar-brand d-lg-none" href="#">
+            <img src="{{asset('images/logo.svg')}}" alt="" style="width: 100%;">
+        </a>
+        <div class="w-100 text-right">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">Bootstrap 4 Cover Nav</a>
-            <div class="collapse navbar-collapse pull-xs-right justify-content-end" id="navbarToggler-1">
-                <ul class="navbar-nav mt-2 mt-md-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Our Work</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-outline-white btn-outline" href="#"><i class="fa fa-user"></i> Login
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="cover-container pb-5">
-        <div class="cover-inner container">
-            <h1 class="jumbotron-heading">Say <em>hello</em> to <strong>BS4 Transparent Navbar cover image</strong></h1>
-            <p class="lead">Ideal for header on your new builds, Clean Code......</p>
-            <p class="lead">Need customized build them contact me</p>
-            <p>
-                <a href="https://plowenltd.info" class="btn btn-primary btn-lg mb-2 mr-2 ml-2">Wiseowl</a>
-                <a href="http://dev.turquoiseltd.com" class="btn btn-outline-white btn-lg mb-2 ml-2 ml-2">New Wordpress build</a>
-            </p>
-            <p class="lead">If you fancy changing the navbar settings then simply change the toggleable-sm to toggleable-md , personally don't take it higher as it looks aweful.</p>
-            <p class="lead">But feel free to change the settings.</p>
         </div>
     </div>
-</section>
+    <div class="collapse navbar-collapse flex-grow-1 text-center" id="myNavbar7">
+        <ul class="navbar-nav ml-auto flex-nowrap">
+            <li class="nav-item">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Potafolio</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Trabaja con nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Contactanos</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><img src="{{asset('images/whatsapp_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><img src="{{asset('images/facebook_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><img src="{{asset('images/insta_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>
+            </li>
+        </ul>
+    </div>
+</nav>
