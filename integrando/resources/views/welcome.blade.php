3527ade24f1709d9cb4f78d4be337dab49769819
Manuel Camacho <maedca@gmail.com>
2019-03-17 23:23:52 -0400

    fixes

--- integrando/resources/views/welcome.blade.php
+++ integrando/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <main role="main" class="container">
     <div class="row">
 
-        <div class="col-sm-6 offset-3">
+        <div class="col-md-6 offset-md-3 col-sm-12">
             <br><br>
             <h1> Líneas Estratégicas de Negocio</h1>
             <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid animi consequatur dolore, dolorum enim explicabo fuga </p>
@@ -25,24 +25,23 @@
             </p>
         </div>
         <div class="col-sm-4">
-             <span class="img-fluid"><img src="{{asset('images/tuercas.svg')}}" alt="" style="width: 30%; display: block;
+             <span class="img-fluid"><img src="{{asset('images/libro.svg')}}" alt="" style="width: 30%; display: block;
   margin-left: auto;
   margin-right: auto;" class="img-fluid"></span>
-            <h3 class="text-center">Sistemas de Gestión</h3>
+            <h3 class="text-center">Apoyo a la Educación</h3>
             <p class="text-center">
                 Contamos con experiencia en el sector
-                educativo, asesorando en temas especí-
-                cos de los ambientes e...
+                educativo, asesorando en temas específicos de los ambientes e...
             </p>
         </div>
         <div class="col-sm-4">
-             <span class="img-fluid"><img src="{{asset('images/tuercas.svg')}}" alt="" style="width: 30%; display: block;
+             <span class="img-fluid"><img src="{{asset('images/luces.svg')}}" alt="" style="width: 30%; display: block;
   margin-left: auto;
   margin-right: auto;" class="img-fluid"></span>
-            <h3 class="text-center">Sistemas de Gestión</h3>
+            <h3 class="text-center">Eventos</h3>
             <p class="text-center">
                 Contamos con experiencia en el sector
-                educativo, asesorando en temas específiwebcos de los ambientes e...
+                educativo, asesorando en temas específicos de los ambientes e...
             </p>
         </div>
     </div>
