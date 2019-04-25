135cc8b17726f43388800e361d9c1daae21a831c
Manuel Camacho <maedca@gmail.com>
2019-04-25 17:05:53 -0400

    cambio

--- integrando/resources/views/layouts/partials/nav2.blade.php
+++ integrando/resources/views/layouts/partials/nav2.blade.php
@@ -1,11 +1,16 @@
-<div>
-<div class="conainer">
+
+<div class="container d-none d-sm-block">
+    <div class="row">
     <br class="d-none d-sm-block">
     {{--<p>Licencia en Seguridad y Salud  en el Trabajo S2019060004999</p>--}}
-<img src="{{asset('images/logo_let_azul.png')}}" alt="" style="width: 10%; transform: translateX(250px) " class="d-none d-sm-block">
+        <br><br>
+    <div class="col-sm-4"><br><img src="{{asset('images/logo_let_azul.png')}}" alt="" style="width: 50%; " class="d-none d-sm-block"><br></div>
+    <div class="col-sm-8"><br><br><br><br><h3 style="color: #262f64">Licencia en Seguridad y Salud en el Trabajo S2019060004999</h3></div>
 
     <br class="d-none d-sm-block">
     <br class="d-none d-sm-block">
+    </div>
+</div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between" style="" data-toggle="sticky-onscroll">
     {{--<a class="navbar-brand" href="#" style="margin-left: 50px"  >Navbar</a>--}}
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -55,44 +60,42 @@
 
     </div>
     {{--<div class="collapse navbar-collapse flex-grow-1 text-center"id="myNavbar7">--}}
-        {{--<ul class="navbar-nav ml-auto flex-nowrap">--}}
-            {{--<li class="nav-item {{ (request()->is('/')) ? 'activo_home' : '' }}">--}}
-                {{--<a href="/" class="nav-link">Home</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item {{ (request()->is('us')) ? 'activo_us' : '' }}">--}}
-                {{--<a href="/us" class="nav-link">Nosotros</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item dropdown {{ (request()->is('line_1')) ? 'activo' : '' }} {{ (request()->is('line_2')) ? 'activo' : '' }} {{ (request()->is('line_3')) ? 'activo' : '' }}">--}}
-                {{--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-                    {{--Portafolio--}}
-                {{--</a>--}}
-                {{--<div class="dropdown-menu" aria-labelledby="navbarDropdown">--}}
-                    {{--<a class="dropdown-item {{ (request()->is('line_1')) ? 'activo_line_1' : '' }}" href="/line_1">Línea Estratégica de Sistemas de Gestión el Trabajo</a>--}}
-                    {{--<a class="dropdown-item  {{ (request()->is('line_2')) ? 'activo_line_2' : '' }}" href="/line_2">Línea Estratégica de Apoyo a la Educación</a>--}}
-                    {{--<a class="dropdown-item  {{ (request()->is('line_3')) ? 'activo_line_3' : '' }}" href="/line_3">Línea Estratégica de Eventos</a>--}}
+    {{--<ul class="navbar-nav ml-auto flex-nowrap">--}}
+    {{--<li class="nav-item {{ (request()->is('/')) ? 'activo_home' : '' }}">--}}
+    {{--<a href="/" class="nav-link">Home</a>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item {{ (request()->is('us')) ? 'activo_us' : '' }}">--}}
+    {{--<a href="/us" class="nav-link">Nosotros</a>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item dropdown {{ (request()->is('line_1')) ? 'activo' : '' }} {{ (request()->is('line_2')) ? 'activo' : '' }} {{ (request()->is('line_3')) ? 'activo' : '' }}">--}}
+    {{--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+    {{--Portafolio--}}
+    {{--</a>--}}
+    {{--<div class="dropdown-menu" aria-labelledby="navbarDropdown">--}}
+    {{--<a class="dropdown-item {{ (request()->is('line_1')) ? 'activo_line_1' : '' }}" href="/line_1">Línea Estratégica de Sistemas de Gestión el Trabajo</a>--}}
+    {{--<a class="dropdown-item  {{ (request()->is('line_2')) ? 'activo_line_2' : '' }}" href="/line_2">Línea Estratégica de Apoyo a la Educación</a>--}}
+    {{--<a class="dropdown-item  {{ (request()->is('line_3')) ? 'activo_line_3' : '' }}" href="/line_3">Línea Estratégica de Eventos</a>--}}
 
-                {{--</div>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item {{ (request()->is('blog')) ? 'activo_us' : '' }}">--}}
-                {{--<a href="/blog" class="nav-link">Blog</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item {{ (request()->is('work')) ? 'activo_work' : '' }}">--}}
-                {{--<a href="/work" class="nav-link">Trabaja con nosotros</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item {{ (request()->is('contact')) ? 'activo_contact' : '' }}">--}}
-                {{--<a href="/contact" class="nav-link">Contactanos</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item">--}}
-                {{--<a href="#" class="nav-link"><img src="{{asset('images/whatsapp_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item">--}}
-                {{--<a href="#" class="nav-link"><img src="{{asset('images/facebook_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item">--}}
-                {{--<a href="#" class="nav-link"><img src="{{asset('images/insta_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>--}}
-            {{--</li>--}}
-        {{--</ul>--}}
+    {{--</div>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item {{ (request()->is('blog')) ? 'activo_us' : '' }}">--}}
+    {{--<a href="/blog" class="nav-link">Blog</a>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item {{ (request()->is('work')) ? 'activo_work' : '' }}">--}}
+    {{--<a href="/work" class="nav-link">Trabaja con nosotros</a>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item {{ (request()->is('contact')) ? 'activo_contact' : '' }}">--}}
+    {{--<a href="/contact" class="nav-link">Contactanos</a>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item">--}}
+    {{--<a href="#" class="nav-link"><img src="{{asset('images/whatsapp_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item">--}}
+    {{--<a href="#" class="nav-link"><img src="{{asset('images/facebook_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>--}}
+    {{--</li>--}}
+    {{--<li class="nav-item">--}}
+    {{--<a href="#" class="nav-link"><img src="{{asset('images/insta_b.svg')}}" alt="" class="img-fluid" style="width: 20px"></a>--}}
+    {{--</li>--}}
+    {{--</ul>--}}
     {{--</div>--}}
 </nav>
-</div>
-</div>
