aeee0661a0849d98d92728ca66851d7c6be8b338
Manuel Camacho <maedca@gmail.com>
2019-06-21 12:41:18 -0400

    seed

--- integrando/database/seeds/UsersTableSeeder.php
+++ integrando/database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
-        factory(\App\User::class, 29)->create();
+//        factory(\App\User::class, 29)->create();
 
         \App\User::create([
             'name'=> 'admin',
